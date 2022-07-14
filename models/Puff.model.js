a1ca65b7dd40e98b8732b50c83e433f4a6b15d99
Mafalda Oliveira <mafalda.oliveira.21@icloud.com>
2022-07-14 11:38:11 +0100

    puff model initial commit

--- models/Puff.model.js
+++ models/Puff.model.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const projectSchema = new Schema({
+const puffSchema = new Schema({
   name: String,
   description: String,
-  tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }],
   // owner will be added later on
 });
 
-module.exports = model("Project", projectSchema);
+module.exports = model("Puff", puffSchema);
